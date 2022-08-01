d1fb41e9b41f363cccdf199edaf34da36552fce6
Diogo Baltazar <ti3@ballke.com.br>
2022-08-01 13:01:18 -0300

    adicionando metodo para deletar hora do ponto e restruturação no datatable

--- src/components/AttendanceDay.js
+++ src/components/AttendanceDay.js
@@ -5,47 +5,101 @@ import {DatePicker} from '@mui/x-date-pickers/DatePicker';
 import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
 import TextField from '@mui/material/TextField';
-import {useEffect, useState} from 'react';
+import {GridActionsCellItem} from "@mui/x-data-grid";
+import DeleteIcon from '@mui/icons-material/Delete';
 
-function RenderAttendanceMonth(values, columns) {
-    const rows = PopulateFields(values.getUTCDate(), values.getMonth() + 1, values.getFullYear())
-    return (<DataTable rows={rows} columns={columns} />
-    )
-}
-
-function PopulateFields(day, month, year) {
-    const [mes, setMes] = useState([])
-    useEffect(() => {
-        fetch(`http://127.0.0.1:8000/attendance/day/${day}/month/${month}/year/${year}`)
-            .then(resposta => resposta.json())
-            .then(dados => {
-                setMes(dados)
-            })
-
-    }, [day])
-    let rows = []
-    if (mes.length) {
-        rows = mes[0].attendance_hour.map(dia => ({
-            id: `${dia.id}_${mes[0].id}`,
-            day: mes[0].day,
-            name: mes[0].employee_shift,
+function FormatRow(dados) {
+    return (
+        dados.attendance_hour.map(dia => ({
+            id: `${dia.id}_${dados.id}`,
+            day: dados.day,
+            name: dados.employee_shift,
             attendance_hour: dia.hour,
         }))
-    }
-    return (
-        rows
     )
 }
 
 export default function AttendanceDay() {
-    const [value, setValue] = React.useState(new Date());
+    const [row, setRow] = React.useState([]);
+    const [date, setDate] = React.useState(new Date);
 
+    const updateAttendancceHour = (attendance_hour) => {
+        var data = {
+            id: attendance_hour.id.split('_')[0],
+            attendance_hour: attendance_hour.value,
+        };
+
+        fetch(`http://127.0.0.1:8000/attendance/day/${attendance_hour.id.split('_')[1]}/`,
+            {
+                method: "patch",
+                credentials: "same-origin",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setRow(FormatRow(dados))
+            })
+    }
+
+    const populateFields = (value) => {
+        const day = value.getUTCDate()
+        const month = value.getMonth() + 1
+        const year = value.getFullYear()
+
+        let attendance = []
+        fetch(`http://127.0.0.1:8000/attendance/day/${day}/month/${month}/year/${year}`)
+            .then(resposta => resposta.json())
+            .then(dados => {
+                if (dados.length) {
+                    setRow(FormatRow(dados[0]))
+                }
+            })
+    }
+
+    const deletedAttendanceHour = React.useCallback(
+        (attendance_hour) => () => {
+            setTimeout(() => {
+                var data = {
+                    id: attendance_hour.id.split('_')[0],
+                };
+
+                fetch(`http://127.0.0.1:8000/attendance/day/${attendance_hour.id.split('_')[1]}/`,
+                    {
+                        method: "delete",
+                        credentials: "same-origin",
+                        headers: {
+                            "Accept": "application/json",
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(data)
+                    })
+                    .then(resposta => resposta.json())
+                    .then(dados => {
+                        console.log(dados)
+                        setRow(FormatRow(dados))
+                    })
+            });
+        },
+        [],
+    );
     const columns = [
-        {field: 'day', headerName: 'Dia', width: 300, type:'date'},
+        {field: 'day', headerName: 'Dia', width: 300, type: 'date'},
         {field: 'name', headerName: 'Nome', width: 300},
         {field: 'attendance_hour', headerName: 'Hora Batida', width: 300, editable: true},
+        {
+            field: 'actions', type: 'actions', width: 80, getActions: (params) => [
+                <GridActionsCellItem
+                    icon={<DeleteIcon/>}
+                    label="Delete"
+                    onClick={deletedAttendanceHour(params)}
+                />
+            ],
+        },
     ];
-
 
     return (
         <Container>
@@ -54,14 +108,15 @@ export default function AttendanceDay() {
                     <DatePicker
                         views={['day']}
                         label="Year, month and date"
-                        value={value}
+                        value={date}
                         onChange={(newValue) => {
-                            setValue(newValue);
+                            setDate(newValue)
+                            populateFields(newValue);
                         }}
                         renderInput={(params) => <TextField {...params} helperText={null}/>}
                     />
                 </LocalizationProvider>
-                {RenderAttendanceMonth(value, columns)}
+                <DataTable rows={row} columns={columns} onCellEditCommit={updateAttendancceHour}/>
             </Grid>
         </Container>
     );
--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,27 +1,6 @@
 import * as React from 'react';
 import {DataGrid} from '@mui/x-data-grid';
 
-function UpdateAttendancceHour(attendance_hour) {
-    var data = {
-        id: attendance_hour.id.split('_')[0],
-        attendance_hour: attendance_hour.value,
-    };
-
-    fetch(`http://127.0.0.1:8000/attendance/day/${attendance_hour.id.split('_')[1]}/`,
-        {
-            method: "patch",
-            credentials: "same-origin",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        })
-        .then(resposta => resposta.json())
-        .then(dados => {
-            console.log(dados)
-        })
-}
 
 export default function DataTable(...props) {
     const rows = props[0].rows
@@ -43,7 +22,7 @@ export default function DataTable(...props) {
                         }}
                         rows={rows}
                         columns={columns}
-                        onCellEditCommit={UpdateAttendancceHour}
+                        onCellEditCommit={props[0].onCellEditCommit}
                         pageSize={5}
                         rowsPerPageOptions={[5]}
                     />
